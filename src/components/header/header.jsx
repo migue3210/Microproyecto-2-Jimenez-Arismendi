166efc5ee9b40758c74e25306b236708f10a36e4
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-03-09 23:11:37 -0400

    Adds links to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import './header.css'
 
 export default function Header() {
@@ -6,8 +7,8 @@ export default function Header() {
             <div className="header">
                 <h1>Gaming Club</h1>
                 <div className="nav">
-                    <a href="" className="link"><li>Inicio</li></a>
-                    <a href="" className="link"><li>Buscar Juegos</li></a>
+                    <Link className="link" to='/inicio'>Inicio</Link >
+                    <Link className="link" to='/buscar-juego'>Buscar Juegos</Link >
                 </div>
             </div>
         </>
