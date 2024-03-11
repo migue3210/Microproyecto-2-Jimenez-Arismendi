9d3f9f7eaf17be110296ffd9a61252c70fb147d5
Monfernus <luis.arismendi@correo.unimet.edu.ve>
2024-03-10 21:44:02 -0400

    Google to landing
    fixed

--- src/components/login/sign-in.component.jsx
+++ src/components/login/sign-in.component.jsx
@@ -1,11 +1,25 @@
 import { signInWithGooglePopup } from "../../services/firebase.util"
 import {GoogleButton} from 'react-google-button';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+    const navigate = useNavigate();
+
     const handleGoogleSignIn = async () => {
+        try {
             const response = await signInWithGooglePopup();
             console.log(response);
-        }
+      
+            // Verifica si el inicio de sesión fue exitoso
+            if (response) {
+              // Redirige a la página deseada
+              navigate('/inicio'); 
+            }
+          } catch (error) {
+            console.error('Error al iniciar sesión con Google:', error);
+          }
+        };
+
     return (
             
           <div className='google'>
